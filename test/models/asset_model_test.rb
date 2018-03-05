4d2b4d3149020830ee6b0738ef954988a9e9b365
kttalley <talleykristian@gmail.com>
2018-03-05 00:16:32 -0800

    Learning scss and path versus url routes

---
+++ test/models/asset_model_test.rb
@@ -0,0 +1,16 @@
+# require 'test_helper'
+
+# class AssetModelTest < ActionDispatch::IntegrationTest
+  
+
+#   test "should get cat" do
+#     get 'app/assets/kitten.jpg'
+    
+#   end
+
+
+#Tests on the controllor are primarily
+#testing if server requests are successful/not
+
+#Tests on the model are checking your
+#local file logic.
